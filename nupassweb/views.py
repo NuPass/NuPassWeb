59eaa15a874169e7c26227330d963776c1adc0ba
Sean Callaway <seancallaway@gmail.com>
2017-01-30 15:26:06 -0800

    Added logic to support quantities for #2.

--- nupassweb/views.py
+++ nupassweb/views.py
@@ -7,12 +7,18 @@
     :copyright: (c) 2017 by Sean Callaway.
     :license: GNU GPL v2, see LICENSE for more details.
 """
-from flask import render_template
+from flask import render_template, request
 from nupassweb import app, nupass
 
 @app.route('/')
 def index():
+    qty = int(request.args.get('qty', '5'))
+    notice = 0
     passes = ""
-    for i in range(0, 5):
+    if qty > 30:
+        qty = 30
+        notice = 2
+        passes = "If more than 30 passwords are required, please rerun the tool or consider using the command-line version.\n\n"        
+    for i in range(0, qty):
         passes = passes + nupass.gen_pass() + "\n"
-    return render_template('index.html', passwords=passes)
+    return render_template('index.html', passwords=passes, numRows=qty+notice)
