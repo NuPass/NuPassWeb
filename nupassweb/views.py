e95ff9aea62d4026bbabe67d3ce9f9ad051056ea
Sean Callaway <seancallaway@gmail.com>
2017-07-05 10:36:39 -0700

    Passwords loaded via jQuery.
    This includes the initial load and refreshing the password list.

--- nupassweb/views.py
+++ nupassweb/views.py
@@ -7,25 +7,35 @@
     :copyright: (c) 2017 by Sean Callaway.
     :license: GNU GPL v2, see LICENSE for more details.
 """
-from flask import render_template, request
-from nupass import gen_pass
+from flask import render_template, request, jsonify
+import nupass
 from nupassweb import app
 
 @app.route('/')
 def index():
-    """Main page view.
+    """Main page view."""
+    return render_template('index.html')
+
+@app.route('/gen_pass')
+def gen_pass():
+    """Generate passwords view.
 
     HTTP GET ARGUMENTS:
     qty -- the number of passwords to generate (default 5)
     """
-    qty = int(request.args.get('qty', '5'))
+
+    pass_list = []
+
+    qty = request.args.get('qty', 5, type=int)
     notice = 0
-    passes = ""
+    notice_str = ""
     if qty > 30:
         qty = 30
         notice = 2
-        passes = "If more than 30 passwords are required, please rerun the " \
+        notice_str = "If more than 30 passwords are required, please rerun the " \
                  "tool or consider using the command-line version.\n\n"
     for _ in range(0, qty):
-        passes = passes + gen_pass() + "\n"
-    return render_template('index.html', passwords=passes, numRows=qty+notice)
+        pass_dict = {'password' : nupass.gen_pass()}
+        pass_list.append(pass_dict)
+
+    return jsonify(rows=notice+qty, notice_str=notice_str, passwords=pass_list)
